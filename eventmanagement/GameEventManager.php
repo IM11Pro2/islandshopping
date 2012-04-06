d80a86c047cee34d52cc484a0506fa4ebca06078
Marvin Schwoiger <marv.schwoiger@gmx.at>
2012-04-06 11:51:56 +0200

    Added Eventmanager

---
+++ eventmanagement/GameEventManager.php
@@ -0,0 +1,51 @@
+<?php
+	
+	class GameEventManager implements IEventManager{
+		
+			private function __construct(){}			
+			
+			
+			private static $instance = null;
+            
+			
+			public static function getInstance(){
+				if(self::$instance == null){
+					self::$instance = new GameEventManager();
+				}
+				
+				return self::$instance;
+			}
+		
+			public function addEventListener($eventListener, $eventType){
+						
+				if(!isset($listeners[$eventType])){
+					$listeners[$eventType] = array($eventListener);
+				}
+				else{
+					array_push($listeners[$eventType], $eventListener);
+				}
+			}
+
+			public function removeEventListener($eventListener, $eventType){
+				
+				
+				for($i = 0; $i < count($listeners[$eventType]); ++$i) {
+					if($listeners[$eventType][i] === $eventListener){
+						unset($listeners[$eventType][i]);
+						return;
+					}
+				}
+				
+			
+								
+			}
+	
+			public function dispatchEvent($event, $listeners){
+				
+				foreach ($listeners[$event->getEventType()] as $listener) {
+					$listener->handleEvent($event);
+				}
+			
+			}
+	}
+?>
---
+++ eventmanagement/IEventManager.php
@@ -0,0 +1,11 @@
+<?php
+interface IEventManager{
+	
+	public function addEventListener($eventListener, $eventType);
+	
+	public function removeEventListener($eventListener);
+	
+	public function dispatchEvent($event);
+
+}
+?>
