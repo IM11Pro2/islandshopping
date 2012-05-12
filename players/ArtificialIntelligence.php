27c3eb4d1148d94f36b403c34927175ba7e2d7e8
Bernadette Zlabinger <berli_1@gmx.at>
2012-05-12 17:43:16 +0200

    Beim Klick auf "NextPlayer" wird der erste Gegner aufgerufen und dessen "makeDecision()" danach gibt er zurück ob und welche region er angreift, bzw welche seiner eigenen regionen er aufstocken möchte

--- players/ArtificialIntelligence.php
+++ players/ArtificialIntelligence.php
@@ -25,4 +25,29 @@
         public function getPlayerId() {
             return $this->playerId;
         }
+
+        public function makeDecision($allEnemyRegions, $regions){
+            for($i=0; $i<count($allEnemyRegions); $i++){
+                $actualRegion = $allEnemyRegions[$i];
+
+
+
+
+                //Look if Neighbour-Attack is possible
+                $neighbours = $actualRegion->getNeighbours();
+                //print_r($neighbours);
+                for($j=0; $j< count($neighbours); $j++){
+                    if($regions[$neighbours[$j]]->getCountry() != $this->getCountry()){
+                        if($regions[$neighbours[$j]]->getPayment()->getValue() < $actualRegion->getPayment()->getValue()){
+                            $regionId = $regions[$neighbours[$j]]->getRegionId();
+                            echo json_encode(array("attack" => $regionId));
+                        }
+                        else{
+                            $regionId = $actualRegion->getRegionId();
+                            echo json_encode(array("payOff" => $regionId));
+                        }
+                    }
+                }
+            }
+        }
     }
