240ccccf72cd7e4de9e172ecc9d0850b525b079b
Bernadette Zlabinger <berli_1@gmx.at>
2012-05-12 19:25:16 +0200

    Beim Klick auf NextPlayer berechnet die KI alle ihre möglichkeiten, wählt zufällig eine aus, und ruft die ensprechenden methoden auf --> Funktioniert noch nicht wegen fehlender bank, aber sonst ok :)

--- states/PlayState.php
+++ states/PlayState.php
@@ -10,6 +10,8 @@
         function init() {
             $playerList = $_SESSION['activePlayers'];
             $this->bank = new Bank($playerList[0]->getCountry(), Bank::ATTACK);
+
+            $_SESSION['state'] = $this;
         }
 
         function endState() {
@@ -20,6 +22,71 @@
 
         function getApplicationStateType() {
             return self::ApplicationStateType;
+        }
+
+        function attackCountry($attackingCountryId, $enemyId){
+            header('Content-type: application/json');
+
+            $regionId = $attackingCountryId;
+
+            $map = $_SESSION['map'];
+            $regions = $map->getRegions();
+
+            $activeRegion = $regions[$regionId];
+            $activePayment = $activeRegion->getPayment();
+
+            $enemyRegion = $regions[$enemyId];
+            $enemyPayment = $enemyRegion->getPayment();
+
+            $hasPlayerWon = $activePayment->isBuyable($enemyPayment);
+
+            if($hasPlayerWon){
+                //  basis value muss noch abgezogen werden
+                $activeRegion->occupyRegion($enemyRegion);
+                $hallo = json_encode( array("activeRegion" => array("hasWon"=> $hasPlayerWon,
+                                                                    "paymentValue" => $activePayment->getValue(),
+                                                                    "currencyTranslation" => $activePayment->getCurrencyTranslation(),
+                                                                    "regionId" => $regionId),
+                                        "enemyRegion" => array(
+                                            "regionId" => $enemyId,
+                                            "regionOfPlayer" => $enemyRegion->getPlayerId(),
+                                            "countryColor" => $enemyRegion->getColor(),
+                                            "paymentValue" => $enemyPayment->getValue(),
+                                            "currencyTranslation" => $enemyPayment->getCurrencyTranslation())
+                                        ));
+
+                echo $hallo;
+            }
+            else{
+                echo json_encode( array("activeRegion" => array("hasWon"=> $hasPlayerWon)));
+            }
+        }
+
+        function spendMoney($regionId){
+            header('Content-type: application/json');
+
+            $map = $_SESSION['map'];
+            $regions = $map->getRegions();
+
+            //if(trim($_GET['bankstate']) == Bank::PAY_OFF) {
+
+                $_SESSION['bank']->payOff($regions[$regionId]->getPayment());
+
+           // }
+/*            else if(trim($_GET['bankstate']) == Bank::DEPOSIT) {
+
+                $_SESSION['bank']->deposit($regions[$regionId]->getPayment());
+            }*/
+
+            $country = $regions[$regionId]->getCountry();
+            $paymentValue = $regions[$regionId]->getPayment()->getValue() * $regions[$regionId]->getPayment()->getCurrencyTranslation();
+
+            $bankCapital = $_SESSION['bank']->getCapital();
+
+            echo json_encode(array("activeRegion"=> $regionId,
+                                   "payment"     => array("value"    => $paymentValue,
+                                                          "currency" => $country->getPayment()->getCurrency()),
+                                   "bankCapital" => $bankCapital));
         }
 
 
@@ -49,73 +116,11 @@
                 }
 
                 if(isset($_GET['getCountry'])) {
-
-                    header('Content-type: application/json');
-
-                    $regionId = trim($_GET['getCountry']);
-
-                    $map = $_SESSION['map'];
-                    $regions = $map->getRegions();
-
-                    if(trim($_GET['bankstate']) == Bank::PAY_OFF) {
-
-                        $_SESSION['bank']->payOff($regions[$regionId]->getPayment());
-
-                    }
-                    else if(trim($_GET['bankstate']) == Bank::DEPOSIT) {
-
-                        $_SESSION['bank']->deposit($regions[$regionId]->getPayment());
-                    }
-
-                    $country = $regions[$regionId]->getCountry();
-                    $paymentValue = $regions[$regionId]->getPayment()->getValue() * $regions[$regionId]->getPayment()->getCurrencyTranslation();
-
-                    $bankCapital = $_SESSION['bank']->getCapital();
-
-                    echo json_encode(array("activeRegion"=> $regionId,
-                                           "payment"     => array("value"    => $paymentValue,
-                                                                  "currency" => $country->getPayment()->getCurrency()),
-                                           "bankCapital" => $bankCapital));
-
+                    $_SESSION['state']->spendMOney(trim($_GET['getCountry']));
                 }
 
                 if(isset($_GET['region']) && isset($_GET['enemy'])){
-                    header('Content-type: application/json');
-
-                    $regionId = trim($_GET['region']);
-                    $enemyId = trim($_GET['enemy']);
-
-                    $map = $_SESSION['map'];
-                    $regions = $map->getRegions();
-
-                    $activeRegion = $regions[$regionId];
-                    $activePayment = $activeRegion->getPayment();
-
-                    $enemyRegion = $regions[$enemyId];
-                    $enemyPayment = $enemyRegion->getPayment();
-
-                    $hasPlayerWon = $activePayment->isBuyable($enemyPayment);
-
-                    if($hasPlayerWon){
-                        //  basis value muss noch abgezogen werden
-                        $activeRegion->occupyRegion($enemyRegion);
-                        $hallo = json_encode( array("activeRegion" => array("hasWon"=> $hasPlayerWon,
-                                                                            "paymentValue" => $activePayment->getValue(),
-                                                                            "currencyTranslation" => $activePayment->getCurrencyTranslation(),
-                                                                            "regionId" => $regionId),
-                                                "enemyRegion" => array(
-                                                    "regionId" => $enemyId,
-                                                    "regionOfPlayer" => $enemyRegion->getPlayerId(),
-                                                    "countryColor" => $enemyRegion->getColor(),
-                                                    "paymentValue" => $enemyPayment->getValue(),
-                                                    "currencyTranslation" => $enemyPayment->getCurrencyTranslation())
-                                                ));
-
-                        echo $hallo;
-                    }
-                    else{
-                        echo json_encode( array("activeRegion" => array("hasWon"=> $hasPlayerWon)));
-                    }
+                    $_SESSION['state']->attackCountry(trim($_GET['region']), trim($_GET['enemy']));
                 }
 
                 if(isset($_GET['nextPlayer'])){
@@ -126,6 +131,7 @@
                     $enemy = $_SESSION['activePlayers'][1];
                     $enemyId = $enemy->getPlayerId();
 
+
                     for($i = 0; $i < count($regions); $i++){
                         if ($regions[$i]->getPlayerId() == $enemyId){
                             array_push($allEnemyRegions, $regions[$i]);
@@ -134,11 +140,22 @@
 
                     //print_r($allEnemyRegions);
 
-                    /*$decision = */$enemy->makeDecision($allEnemyRegions, $regions);
+                    $decision = $enemy->makeDecision($allEnemyRegions, $regions);
+
+                    print_r($decision);
+
+                    if(array_key_exists("attack", $decision)){
+                        //$_SESSION['state']->attackCountry($decision[0], $decision[1]);
+                    }
+                    else if (array_key_exists("payOff", $decision)){
+                        $_SESSION['state']->spendMOney($decision["payOff"]);
+                    }
                 }
             }
         }
     }
+
+
 
     if(isset($_GET['handle']) && trim($_GET['handle']) == "PlayState") {
 
